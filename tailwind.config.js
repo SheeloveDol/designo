4ad81b8aae3cfa3c27ba7a10fbb23e539f2263cd
Sheelove Dol <siesel26@gmail.com>
2023-05-31 15:31:42 -0700

    Deleted unnecessary files and set up global font

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,22 @@
 /** @type {import('tailwindcss').Config} */
+
+
 export default {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
+    colors: {
+      'peach' : 'rgb(231, 129, 107)',
+      'lightPeach' : 'rgb(255, 173, 155)',
+      'black' : 'rgb(29, 28, 30)',
+      'darkGrey' : 'rgb(51, 49, 54)',
+      'white' : 'rgb(255, 255, 255)',
+      'lightGrey' : 'rgb(241, 243, 245)',
+    },
+
+
     extend: {},
   },
   plugins: [],
