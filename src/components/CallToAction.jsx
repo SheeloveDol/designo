ccbf7def0f8bc4add35db849ac318104f628e646
Sheelove Dol <siesel26@gmail.com>
2023-09-29 16:45:12 -0700

    refactored CTA into its own component

---
+++ src/components/CallToAction.jsx
@@ -0,0 +1,30 @@
+/* eslint-disable react/no-unescaped-entities */
+import CtaBackgroundImg from "../assets/shared/mobile/bg-pattern-design-pages-intro-mobile.svg"
+
+export const CallToAction = () => {
+  return (
+    <div>
+      <div
+        className="text-center text-white py-8 lg:py-16 px-4  absolute top-3 w-[90%] left-[5%] bg-peach rounded-lg z-10 lg:flex justify-between lg:px-16 lg:text-left"
+        style={{
+          backgroundImage: `url('${CtaBackgroundImg}')`,
+
+          backgroundPosition: "center",
+        }}
+      >
+        <div className="sm:mx-auto lg:mx-0 sm:w-3/4 lg:w-1/2">
+          <h3 className="mb-6 text-2xl">
+            Let's talk about<br></br> your project
+          </h3>
+          <p className="mb-6 ">
+            Ready to take it to the next level? Contact us today and find out
+            how our expertise can help your business grow.
+          </p>
+        </div>
+        <button className="text-black bg-lightGrey p-4 rounded-lg lg:flex-none h-14 self-center hover:bg-lightPeach hover:text-white">
+          GET IN TOUCH
+        </button>
+      </div>
+    </div>
+  );
+};
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -7,7 +7,7 @@ import Youtube from "../assets/shared/desktop/icon-youtube.svg";
 import Twitter from "../assets/shared/desktop/icon-twitter.svg";
 import Pinterest from "../assets/shared/desktop/icon-pinterest.svg";
 import Instagram from "../assets/shared/desktop/icon-instagram.svg";
-import CtaBackgroundImg from "../assets/shared/mobile/bg-pattern-design-pages-intro-mobile.svg"
+import { CallToAction } from "./CallToAction";
 
 export const Footer = () => {
   const location = useLocation();
@@ -34,27 +34,7 @@ export const Footer = () => {
     >
       <div className="bg-white py-16 pt-28 sm:pt-44 relative"></div>
 
-      <div className="text-center text-white py-8 lg:py-16 px-4  absolute top-3 w-[90%] left-[5%] bg-peach rounded-lg z-10 lg:flex justify-between lg:px-16 lg:text-left"
-      style={{
-        backgroundImage: `url('${CtaBackgroundImg}')`,
-        
-        backgroundPosition: "center"
-      }}
-      
-      >
-        <div className="sm:mx-auto lg:mx-0 sm:w-3/4 lg:w-1/2">
-          <h3 className="mb-6 text-2xl">
-            Let's talk about<br></br> your project
-          </h3>
-          <p className="mb-6 ">
-            Ready to take it to the next level? Contact us today and find out how
-            our expertise can help your business grow.
-          </p>
-        </div>
-        <button className="text-black bg-lightGrey p-4 rounded-lg lg:flex-none h-14 self-center hover:bg-lightPeach hover:text-white">
-          GET IN TOUCH
-        </button>
-      </div>
+      <CallToAction />
 
       <div>
         <div className="sm:mx-10 lg:mx-16 pt-36 sm:pt-20  flex flex-col items-center just gap-10">
