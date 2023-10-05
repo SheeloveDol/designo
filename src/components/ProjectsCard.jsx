bb557b0f5375253e59e6514a2bb97df197a5f47b
Sheelove Dol <siesel26@gmail.com>
2023-10-05 15:19:25 -0700

    Fixed height of ProjectCards

--- src/components/ProjectsCard.jsx
+++ src/components/ProjectsCard.jsx
@@ -4,13 +4,13 @@
 export const ProjectsCard = ({ title, description, image }) => {
   return (
     <div>
-        <div className="bg-[#FFEAE6] flex flex-col sm:grow sm:flex-row rounded-lg lg:flex-col hover:cursor-pointer group  last:text-peach hover:text-white hover:bg-peach">
+        <div className="bg-[#FFEAE6] flex flex-col h-full sm:grow sm:flex-row rounded-lg lg:flex-col hover:cursor-pointer group  last:text-peach hover:text-white hover:bg-peach">
                 <div className="sm:basis-1/2">
                     <img 
                         className="rounded-t-lg sm:rounded-l-lg lg:rounded-t-lg lg:rounded-bl-none  "
                         src={image} alt={`Picture of a ${title} website`}/>
                 </div>
-                <div className="text-center sm:px-4 w-7/10 px-6 lg:w-4/5 mx-auto py-5 sm:basis-1/2 sm:my-auto">
+                <div className="text-center sm:px-4 w-7/10 px-6 lg:w-full mx-auto py-5 sm:basis-1/2 sm:my-auto ">
                     <div>
                         <h4 className="pb-3">{title}</h4>
                     </div>
