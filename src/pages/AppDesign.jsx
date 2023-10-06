2284a526fc0615b1b485f9e6578d2448d0fd5d8c
Sheelove Dol <siesel26@gmail.com>
2023-10-06 09:30:37 -0700

    Added correct data for AppDesignData and GraphicDesignData

--- src/pages/AppDesign.jsx
+++ src/pages/AppDesign.jsx
@@ -64,8 +64,8 @@ return (
     }}
   >
     <div className="px-4 w-9/10 sm:w-4/5 md:w-3/5 lg:w-1/2 mx-auto">
-        <h2 className="text-3xl sm:text-4xl md:text-5xl pt-16 pb-8">Web Design</h2>
-        <p className="pb-16 w-9/10">We build websites that serve as powerful marketing tools and bring memorable brand experiences.</p>
+        <h2 className="text-3xl sm:text-4xl md:text-5xl pt-16 pb-8">App Design</h2>
+        <p className="pb-16 w-9/10">Our mobile designs bring intuitive digital solutions to your customers right at their fingertips.</p>
     </div>
   </section>
 
--- src/pages/GraphicDesign.jsx
+++ src/pages/GraphicDesign.jsx
@@ -53,8 +53,8 @@ export const GraphicDesign = () => {
       }}
     >
       <div className="px-4 w-9/10 sm:w-4/5 md:w-3/5 lg:w-1/2 mx-auto">
-          <h2 className="text-3xl sm:text-4xl md:text-5xl pt-16 pb-8">Web Design</h2>
-          <p className="pb-16 w-9/10">We build websites that serve as powerful marketing tools and bring memorable brand experiences.</p>
+          <h2 className="text-3xl sm:text-4xl md:text-5xl pt-16 pb-8">Graphic Design</h2>
+          <p className="pb-16 w-9/10">We deliver eye-catching branding materials that are tailored to meet your business objectives.</p>
       </div>
     </section>
   
