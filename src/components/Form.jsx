9fd10813784834d1bf900002f97c90adb294acaa
Sheelove Dol <siesel26@gmail.com>
2023-10-11 23:47:18 -0700

    Added react hook form for contact page

---
+++ src/components/Form.jsx
@@ -0,0 +1,118 @@
+import { useForm, Controller } from 'react-hook-form';
+
+const MyForm = () => {
+  const { control, handleSubmit, formState, reset } = useForm();
+  const onSubmit = (data) => {
+    // You can handle form submission here
+    // console.log(data);
+    alert(`Hi ${data.name}, thanks for getting in touch. We will get back to you within 24 hours.`)
+    reset();
+  };
+
+  return (
+    <form onSubmit={handleSubmit(onSubmit)} className=''>
+      <div className="relative border-b hover:border-b-2">
+        <Controller
+          name="name"
+          control={control}
+          defaultValue=""
+          rules={{ required: 'Name is required' }}
+          render={({ field }) => (
+            <input
+              type="text"
+              {...field}
+              className="w-full p-2  bg-peach bg-opacity-0 text-sm placeholder-white/70  placeholder- border-none outline-none "
+              placeholder="Name"
+            />
+          )} 
+        />
+        {formState.errors.name && (
+          <span className="text-red-500 absolute inset-y-0 right-0 pr-2">
+            {formState.errors.name.message}
+          </span>
+        )}
+      </div>
+
+      <div className="relative border-b hover:border-b-2">
+        <Controller
+          name="email"
+          control={control}
+          defaultValue=""
+          rules={{
+            required: 'Email is required',
+            pattern: {
+              value: /^\S+@\S+\.\S+$/,
+              message: 'Invalid email format',
+            },
+          }}
+          render={({ field }) => (
+            <input
+              type="email"
+              {...field}
+              className="w-full p-2 bg-peach text-sm placeholder-white/70 border-none outline-none"
+              placeholder="Email"
+            />
+          )}
+        />
+        {formState.errors.email && (
+          <span className="text-red-500 absolute inset-y-0 right-0 pr-2">
+            {formState.errors.email.message}
+          </span>
+        )}
+      </div>
+
+      <div className="relative border-b hover:border-b-2">
+        <Controller
+          name="phone"
+          control={control}
+          defaultValue=""
+          rules={{ required: 'Phone is required' }}
+          render={({ field }) => (
+            <input
+              type="tel"
+              {...field}
+              className="w-full p-2 bg-peach text-sm placeholder-white/70 border-none outline-none"
+              placeholder="Phone"
+            />
+          )}
+        />
+        {formState.errors.phone && (
+          <span className="text-red-500 absolute inset-y-0 right-0 pr-2">
+            {formState.errors.phone.message}
+          </span>
+        )}
+      </div>
+
+      <div className="relative border-b hover:border-b-2">
+        <Controller
+          name="message"
+          control={control}
+          defaultValue=""
+          rules={{ required: 'Message is required' }}
+          render={({ field }) => (
+            <textarea
+              {...field}
+              className="w-full p-2 bg-peach text-sm placeholder-white/70 border-none outline-none"
+              placeholder="Message"
+            />
+          )}
+        />
+        {formState.errors.message && (
+          <span className="text-red-500 absolute inset-y-0 right-0 pr-2">
+            {formState.errors.message.message}
+          </span>
+        )}
+      </div>
+        <div className='text-center lg:text-right'>
+            <button 
+                type="submit"
+                className='bg-white text-black text-sm mt-8 mb-10 py-2 px-8 rounded-lg hover:bg-lightPeach hover:text-white'
+                >
+                    SUBMIT
+                </button>
+        </div>
+    </form>
+  );
+};
+
+export default MyForm;
--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,7 +1,72 @@
+/* eslint-disable react/no-unescaped-entities */
+
+import ScrollToTop from "../components/ScrollToTop";
+import CanadaImg from "../assets/shared/desktop/illustration-canada.svg";
+import AustraliaImg from "../assets/shared/desktop/illustration-australia.svg";
+import UnitedKingdomImg from "../assets/shared/desktop/illustration-united-kingdom.svg";
+import { LocationCard } from "../components/LocationCard";
+import Form from "../components/Form";
 
 
 export const Contact = () => {
+  const locationCardsData = [
+    {
+      title: "CANADA",
+      image: `${CanadaImg}`,
+      imageRotation: "-rotate-90",
+      bgRotation: "rotate-90",
+      bottomMargin: "mb-16",
+    },
+    {
+      title: "AUSTRALIA",
+      image: `${AustraliaImg}`,
+      imageRotation: "",
+      bgRotation: "",
+      bottomMargin: "mb-16",
+    },
+    {
+      title: "UNITED KINGDOM",
+      image: `${UnitedKingdomImg}`,
+      imageRotation: "rotate-90",
+      bgRotation: "-rotate-90",
+      bottomMargin: "",
+    },
+  ];
+
+  
+
+  
+
   return (
-    <div>Contact</div>
-  )
-}
+    <div className="sm:mx-10 lg:mx-16">
+      <ScrollToTop />
+      <section className="bg-peach bg-smallCircle bg-no-repeat  text-white pt-10 px-6 sm:px-10 lg:flex lg:gap-8 lg:pt-8 lg:px-16 sm:rounded-lg">
+        <div className="lg:basis-1/2 text-center mb-6 sm:text-left lg:pr-6 lg:pt-12">
+          <h2 className="text-2xl sm:text-3xl pb-3">Contact Us</h2>
+          <p className="text-xs">
+            Ready to take it to the next level? Let's talk about your project or
+            idea and find out how we can help your business grow. If you are
+            looking for unique digital experiences that's relatable to your
+            users, drop us a line.
+          </p>
+        </div>
+        <div className="lg:basis-1/2">
+          <Form />
+        </div>
+      </section>
+
+      <section className="pt-24 grid lg:grid-cols-3">
+        {locationCardsData.map((item, index) => (
+          <LocationCard
+            key={index}
+            title={item.title}
+            image={item.image}
+            imageRotation={item.imageRotation}
+            bgRotation={item.bgRotation}
+            bottomMargin={item.bottomMargin}
+          />
+        ))}
+      </section>
+    </div>
+  ); 
+};
