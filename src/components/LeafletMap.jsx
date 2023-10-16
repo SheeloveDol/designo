1979d4c1711c11b37d2e43145fc39ffee81fdc56
Sheelove Dol <siesel26@gmail.com>
2023-10-16 14:58:21 -0700

    Added marker for Leaflet map

--- src/components/LeafletMap.jsx
+++ src/components/LeafletMap.jsx
@@ -1,8 +1,15 @@
 /* eslint-disable react/prop-types */
 import { MapContainer, TileLayer, Marker } from "react-leaflet";
+import { Icon } from "leaflet";
 import "leaflet/dist/leaflet.css";
 
+
 export const LeafletMap = ({ position }) => {
+  const markerIcon = new Icon({
+    iconUrl: "https://cdn-icons-png.flaticon.com/128/2776/2776067.png",
+    iconSize: [38, 38]
+  })
+
   return (
     <div className=" h-72 lg:w-[25rem]  w-full sm:rounded-lg lg:rounded-lg">
       <MapContainer
@@ -14,7 +21,7 @@ export const LeafletMap = ({ position }) => {
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
-        <Marker position={position}></Marker>
+        <Marker position={position} icon={markerIcon} ></Marker>
       </MapContainer>
     </div>
   );
